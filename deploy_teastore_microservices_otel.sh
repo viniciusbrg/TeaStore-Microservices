93e2ef46a9289b90cd3e16614af67a55500de42c
Anderson Almada <andersonalmadagomes@gmail.com>
2023-09-27 16:06:54 +0000

    update

--- deploy_teastore_microservices_otel.sh
+++ deploy_teastore_microservices_otel.sh
@@ -2,9 +2,12 @@
 
 APP_CLUSTER=$(aws eks update-kubeconfig --name app-cluster | sed -e 's/Added new context //g' -e 's/ to .*//g' -e 's/Updated context //g' -e 's/ in .*//g')
 LOADTEST_CLUSTER=$(aws eks update-kubeconfig --name loadtester-cluster | sed -e 's/Added new context //g' -e 's/ to .*//g' -e 's/Updated context //g' -e 's/ in .*//g')
+BACKEND_CLUSTER=$(aws eks update-kubeconfig --name backend-cluster | sed -e 's/Added new context //g' -e 's/ to .*//g' -e 's/Updated context //g' -e 's/ in .*//g')
+
+kubectl config use-context $BACKEND_CLUSTER
+kubectl apply -f microservices/examples/kubernetes/otel-manifests/backend/
 
 kubectl config use-context $APP_CLUSTER
-
 kubectl apply -f microservices/examples/kubernetes/otel-manifests/
 kubectl apply -f microservices/examples/kubernetes/teastore-ribbon-otel.yaml
 
