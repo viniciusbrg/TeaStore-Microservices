19dd76119877ab264cc5e3772886f977d57b9f0c
Vinicius Bernardo Gabriel <vinibern5@gmail.com>
2023-09-26 21:09:49 -0300

    Initial attempt to make monolithic teastore run with otel

---
+++ deploy_teastore_monolithic_otel.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+APP_CLUSTER=$(aws eks update-kubeconfig --name app-cluster | sed -e 's/Added new context //g' -e 's/ to .*//g' -e 's/Updated context //g' -e 's/ in .*//g')
+LOADTEST_CLUSTER=$(aws eks update-kubeconfig --name loadtester-cluster | sed -e 's/Added new context //g' -e 's/ to .*//g' -e 's/Updated context //g' -e 's/ in .*//g')
+
+kubectl config use-context $APP_CLUSTER
+
+kubectl apply -f microservices/examples/kubernetes/otel-manifests/
+kubectl apply -f monolith/examples/kubernetes/k8s-otel.yml
+
+echo "====== IP to Access TeaStore ======"
+
+kubectl describe nodes | grep ExternalIP
+
+echo "==================================="
+
+kubectl config use-context $LOADTEST_CLUSTER
+
+kubectl apply -f microservices/examples/locust-kubernetes/
+
+echo "====== IP to Access Locust ======"
+
+kubectl describe nodes | grep ExternalIP
+
+echo "==================================="
