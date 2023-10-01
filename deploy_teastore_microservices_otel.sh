942b6e14a246698458409d30f56c7db32a8103ed
Vinicius Bernardo Gabriel <vinibern5@gmail.com>
2023-10-01 00:25:24 -0300

    Automate discovery for prometheus scrape url for the collector

--- deploy_teastore_microservices_otel.sh
+++ deploy_teastore_microservices_otel.sh
@@ -4,8 +4,18 @@ APP_CLUSTER=$(aws eks update-kubeconfig --name app-cluster | sed -e 's/Added new
 LOADTEST_CLUSTER=$(aws eks update-kubeconfig --name loadtester-cluster | sed -e 's/Added new context //g' -e 's/ to .*//g' -e 's/Updated context //g' -e 's/ in .*//g')
 BACKEND_CLUSTER=$(aws eks update-kubeconfig --name backend-cluster | sed -e 's/Added new context //g' -e 's/ to .*//g' -e 's/Updated context //g' -e 's/ in .*//g')
 
+kubectl config use-context $APP_CLUSTER
+PROM_COLLECTOR_IP=$(kubectl describe nodes | grep -m1 ExternalIP | cut -d ":" -f2)
+
+cp ./microservices/examples/kubernetes/otel-manifests/backend/prometheus-config.yml ./
+
+cat ./microservices/examples/kubernetes/otel-manifests/backend/prometheus-config.yml | \
+sed 's#\${PROM_COLLECTOR_URL_PLACEHOLDER}'"#${PROM_COLLECTOR_IP}#g" > ./microservices/examples/kubernetes/otel-manifests/backend/prometheus-config.yml
+
 kubectl config use-context $BACKEND_CLUSTER
 kubectl apply -f microservices/examples/kubernetes/otel-manifests/backend/
+
+mv ./prometheus-config.yml ./microservices/examples/kubernetes/otel-manifests/backend/
 
 OTEL_BACKEND_IP=$(kubectl describe nodes | grep -m1 ExternalIP | cut -d ":" -f2)
 
