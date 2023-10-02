783d2af345ddb728f4914531090ed2f367555ba7
Anderson Almada <andersonalmadagomes@gmail.com>
2023-10-02 13:30:36 +0000

    fix destroy

--- destroy_teastore_microservices.sh
+++ destroy_teastore_microservices.sh
@@ -4,10 +4,11 @@ APP_CLUSTER=$(aws eks update-kubeconfig --name app-cluster | sed -e 's/Added new
 LOADTEST_CLUSTER=$(aws eks update-kubeconfig --name loadtester-cluster | sed -e 's/Added new context //g' -e 's/ to .*//g' -e 's/Updated context //g' -e 's/ in .*//g')
 
 kubectl config use-context $APP_CLUSTER
-kubectl delete -f microservices/examples/kubernetes/otel-manifests/
-kubectl delete -f microservices/examples/kubernetes/teastore-ribbon-otel.yaml
+
+kubectl delete -f microservices/examples/kubernetes/teastore-ribbon.yaml
 
 kubectl config use-context $LOADTEST_CLUSTER
+
 kubectl delete -f microservices/examples/locust-kubernetes/
 
 echo "==================================="
