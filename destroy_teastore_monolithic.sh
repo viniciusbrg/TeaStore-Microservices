3c0e2300d8ac7decf93c9d5a73555b51aa1856d2
Your Name <you@example.com>
2023-10-05 13:26:35 +0000

    update script

---
+++ destroy_teastore_monolithic.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+APP_CLUSTER=$(aws eks update-kubeconfig --name app-cluster | sed -e 's/Added new context //g' -e 's/ to .*//g' -e 's/Updated context //g' -e 's/ in .*//g')
+LOADTEST_CLUSTER=$(aws eks update-kubeconfig --name loadtester-cluster | sed -e 's/Added new context //g' -e 's/ to .*//g' -e 's/Updated context //g' -e 's/ in .*//g')
+
+kubectl config use-context $APP_CLUSTER
+
+kubectl delete -f monolith/examples/kubernetes/db.yml
+kubectl delete -f monolith/examples/kubernetes/k8s.yml
+
+kubectl config use-context $LOADTEST_CLUSTER
+
+kubectl delete -f microservices/examples/locust-kubernetes/
+
+echo "==================================="
