8d88f54c0c3ea5c696c9ae898924309dbaa95488
Your Name <you@example.com>
2023-10-05 14:07:33 +0000

    ok

--- deploy_teastore_monolithic_otel.sh
+++ deploy_teastore_monolithic_otel.sh
@@ -2,12 +2,20 @@
 
 APP_CLUSTER=$(aws eks update-kubeconfig --name app-cluster | sed -e 's/Added new context //g' -e 's/ to .*//g' -e 's/Updated context //g' -e 's/ in .*//g')
 LOADTEST_CLUSTER=$(aws eks update-kubeconfig --name loadtester-cluster | sed -e 's/Added new context //g' -e 's/ to .*//g' -e 's/Updated context //g' -e 's/ in .*//g')
+BACKEND_CLUSTER=$(aws eks update-kubeconfig --name backend-cluster | sed -e 's/Added new context //g' -e 's/ to .*//g' -e 's/Updated context //g' -e 's/ in .*//g')
+
+kubectl config use-context $BACKEND_CLUSTER
+kubectl apply -f microservices/examples/kubernetes/otel-manifests/backend/
+
+echo "====== IP to Access Backend ======"
+
+kubectl describe nodes | grep ExternalIP
+
+echo "==================================="
 
 kubectl config use-context $APP_CLUSTER
 
 kubectl apply -f microservices/examples/kubernetes/otel-manifests/
-kubectl apply -f monolith/examples/kubernetes/db.yml
-sleep 1
 kubectl apply -f monolith/examples/kubernetes/k8s-otel.yml
 
 echo "====== IP to Access TeaStore ======"
