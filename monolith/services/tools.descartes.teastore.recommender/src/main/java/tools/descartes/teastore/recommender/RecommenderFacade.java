f2799e7145a9b8320d81b91a26c83af7c69b691d
Vinicius Bernardo Gabriel <vinibern5@gmail.com>
2023-10-08 18:33:15 -0300

    Add annotations to recommender

--- monolith/services/tools.descartes.teastore.recommender/src/main/java/tools/descartes/teastore/recommender/RecommenderFacade.java
+++ monolith/services/tools.descartes.teastore.recommender/src/main/java/tools/descartes/teastore/recommender/RecommenderFacade.java
@@ -1,5 +1,6 @@
 package tools.descartes.teastore.recommender;
 
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 import tools.descartes.teastore.entities.OrderItem;
 import tools.descartes.teastore.recommender.rest.RecommendEndpoint;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 public class RecommenderFacade {
 
+    @WithSpan
     public static List<Long> getRecommendations(List<OrderItem> items, Long userId) {
         return new RecommendEndpoint().recommend(items, userId);
     }
