d0cc463d3ccc13846b64db2aea9d49f3f22bd5bc
Vinicius Bernardo Gabriel <vinibern5@gmail.com>
2023-10-08 18:32:06 -0300

    Add withspan annotations to persistence

--- monolith/services/tools.descartes.teastore.persistence/src/main/java/tools/descartes/teastore/persistence/PersistenceFacade.java
+++ monolith/services/tools.descartes.teastore.persistence/src/main/java/tools/descartes/teastore/persistence/PersistenceFacade.java
@@ -1,5 +1,6 @@
 package tools.descartes.teastore.persistence;
 
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Response;
 import tools.descartes.teastore.entities.*;
@@ -31,6 +32,7 @@ public class PersistenceFacade {
      *             socket timeouts.
      * @return The entity; null if it does not exist.
      */
+    @WithSpan
     public static Product getProduct(Service service, String endpointURI, Class<Product> entityClass, long id)
             throws NotFoundException, LoadBalancerTimeoutException {
         ProductEndpoint pedp = new ProductEndpoint();
@@ -38,16 +40,19 @@ public class PersistenceFacade {
         return pedp.findEntityById(id);
     }
 
+    @WithSpan
     public static Category getCategory(Service service, String endpointURI, Class<Category> entityClass, long id)
             throws NotFoundException, LoadBalancerTimeoutException {
         return new CategoryEndpoint().findEntityById(id);
     }
 
+    @WithSpan
     public static User getUserById(long id)
             throws NotFoundException, LoadBalancerTimeoutException {
         return new UserEndpoint().findEntityById(id);
     }
 
+    @WithSpan
     public static int getProductCountForCategory(Long categoryId) {
         var result = (String) new ProductEndpoint().countForCategory(categoryId).getEntity();
 
@@ -55,38 +60,47 @@ public class PersistenceFacade {
     }
 
 
+    @WithSpan
     public static List<Product> getProductsWithFilters(long categoryID, int startPosition, int numberProducts) {
         return new ProductEndpoint().listAllForCategory(categoryID, startPosition, numberProducts);
     }
 
+    @WithSpan
     public static List<Category> getAllCategories() {
         return new CategoryEndpoint().listAllEntities(-1, -1);
     }
 
+    @WithSpan
     public static List<Order> getAllOrdersFromUser(Long userId) {
         return new OrderEndpoint().listAllForUser(userId, -1, -1);
     }
 
+    @WithSpan
     public static List<Order> getAllOrders() {
         return new OrderEndpoint().listAllEntities(-1, -1);
     }
 
+    @WithSpan
     public static List<OrderItem> getAllOrdemItems() {
         return new OrderItemEndpoint().listAllEntities(-1, -1);
     }
 
+    @WithSpan
     public static long placeNewOrder(Order order) {
         return new OrderEndpoint().createEntity(order);
     }
 
+    @WithSpan
     public static long placeNewOrderItem(OrderItem orderItem) {
         return new OrderItemEndpoint().createEntity(orderItem);
     }
 
+    @WithSpan
     public static User getUserByName(String name) {
         return (User) new UserEndpoint().findById(name).getEntity();
     }
 
+    @WithSpan
     public static Response generateDatabase(Integer categories,
                                             Integer products,
                                             Integer users,
@@ -94,5 +108,6 @@ public class PersistenceFacade {
         return new DatabaseGenerationEndpoint().generateDataBase(categories, products, users, orders);
     }
 
+    @WithSpan
     public static Response generateDatabase() { return generateDatabase(null, null, null, null); }
 }
