4fd1068b5c6e95ee9723845269cdf5584b4623a9
Vinicius Bernardo Gabriel <vinibern5@gmail.com>
2023-10-15 18:10:44 -0300

    update deployscript

--- deploy_cluster.sh
+++ deploy_cluster.sh
@@ -20,7 +20,7 @@ LOCUST_INSTANCE_TYPE="${LOCUST_INSTANCE_TYPE:-c4.xlarge}"
 # Get the first 4 subnet ids from the 4 first AZ's (sorted by name like az-a, az-b, az-c .. )
 SUBNET_IDS=$(aws ec2 describe-subnets | jq '.Subnets | sort_by(.AvailabilityZone) | .[:4] | [ .[] .SubnetId ] | join(",")')
 
-aws cloudformation create-stack --stack-name teastorestack --template-body file://cloudformation.yml \
+aws cloudformation create-stack --stack-name teastorestack --template-body file://cloudformation_otel.yml \
 --parameters ParameterKey=ClustersSubnetIds,ParameterValue=$SUBNET_IDS \
 ParameterKey=TeastoreInstanceType,ParameterValue=$TEASTORE_INSTANCE_TYPE \
 ParameterKey=TeastoreInstances,ParameterValue=$TEASTORE_INSTANCES \
