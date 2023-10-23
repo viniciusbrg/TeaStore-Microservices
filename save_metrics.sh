c1e6b0593d54d5c64ca0250d80799ab670c2134a
Vinicius Bernardo Gabriel <vinibern5@gmail.com>
2023-10-23 10:12:31 -0300

    save network

--- save_metrics.sh
+++ save_metrics.sh
@@ -12,3 +12,22 @@ aws cloudwatch get-metric-statistics --namespace AWS/EC2 --metric-name CPUUtiliz
  --period 300 \
  --region us-east-1 \
  | jq -r '.Datapoints[] | [.Timestamp, .Average, .Unit] | @csv' > $PREFIX-$INSTANCE_ID-cpu.csv
+
+aws cloudwatch get-metric-statistics --namespace AWS/EC2 --metric-name NetworkIn \
+ --dimensions Name=InstanceId,Value=$INSTANCE_ID \
+ --statistics Average \
+ --start-time $(date -d 'now -45 minutes' +"%Y-%m-%dT%H:%M:%S%z") \
+ --end-time $(date +"%Y-%m-%dT%H:%M:%S%z") \
+ --period 300 \
+ --region us-east-1 \
+ | jq -r '.Datapoints[] | [.Timestamp, .Average, .Unit] | @csv' > $PREFIX-$INSTANCE_ID-networkin.csv
+
+aws cloudwatch get-metric-statistics --namespace AWS/EC2 --metric-name NetworkOut \
+ --dimensions Name=InstanceId,Value=$INSTANCE_ID \
+ --statistics Average \
+ --start-time $(date -d 'now -45 minutes' +"%Y-%m-%dT%H:%M:%S%z") \
+ --end-time $(date +"%Y-%m-%dT%H:%M:%S%z") \
+ --period 300 \
+ --region us-east-1 \
+ | jq -r '.Datapoints[] | [.Timestamp, .Average, .Unit] | @csv' > $PREFIX-$INSTANCE_ID-networkout.csv
+
